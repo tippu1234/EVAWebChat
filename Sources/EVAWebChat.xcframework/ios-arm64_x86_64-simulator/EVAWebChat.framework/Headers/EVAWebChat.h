dd23d705a5440faa3991fcd0f3e5a2c50fa2aba1
tippu1234 <raghavendra@MacBookPro.lan>
2025-03-13 15:03:40 +0530

    Initial Commit

---
+++ Sources/EVAWebChat.xcframework/ios-arm64_x86_64-simulator/EVAWebChat.framework/Headers/EVAWebChat.h
@@ -0,0 +1,23 @@
+//
+//  EVAWebChat.h
+//  EVAWebChat
+//
+//  Created by Raghavendra on 16/04/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <EVAWebChat/OSEImageQualityDetector.h>
+#import <EVAWebChat/EVAWebChatWrapper.h>
+
+//! Project version number for EVAWebChat.
+FOUNDATION_EXPORT double EVAWebChatVersionNumber;
+
+//! Project version string for EVAWebChat.
+FOUNDATION_EXPORT const unsigned char EVAWebChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebChat/PublicHeader.h>
+
+
+
+
+
